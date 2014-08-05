07723f89aadab91bbae2e2d35c79c009f436f290
西村 大地 <nishimura.daiji@hpsys.co.jp>
2014-08-05 10:04:40 +0900

    OSX専用のアプリとそうでないものを区別してlinuxbrewでも共通で使用できるスクリプトにした（つもり）

--- util/Brewfile.sh
+++ util/Brewfile.sh
@@ -19,16 +19,16 @@ brew install python
 
 # terminal apps
 brew install git mercurial
-brew install reattach-to-user-namespace
 brew install z zsh tmux ctags ricty
 brew install cmake
 brew install android ant apktool
-brew install terminal-notifier
 brew install the_silver_searcher
 
 brew install vim --with-lua --with-luajit
 
 if [[ $OSTYPE == darwin* ]]; then
+    brew install terminal-notifier
+    brew install reattach-to-user-namespace
     brew install macvim --with-cscope --with-luajit
 
     ln -s /usr/local/opt/macvim/MacVim.app ~/Applications/
@@ -47,39 +47,45 @@ brew install brew-cask
 brew cask update
 
 brew cask install google-chrome
-brew cask install iterm2
-brew cask install xtrafinder
-brew cask install dash
-brew cask install android-file-transfer
 brew cask install android-studio
 brew cask install intellij-idea-ce
-brew cask install atom
 brew cask install sourcetree
 brew cask install genymotion
-brew cask install sequel-pro
-brew cask install rcdefaultapp
 brew cask install virtualbox
 brew cask install synergy
 brew cask install vlc
-brew cask install menumeters
-#brew cask install cheatsheet
+
+if [[ $OSTYPE == darwin* ]]; then
+    brew cask install iterm2
+    brew cask install xtrafinder
+    brew cask install menumeters
+    brew cask install sequel-pro
+    brew cask install rcdefaultapp
+    brew cask install atom
+    brew cask install android-file-transfer
+    brew cask install dash
+    #brew cask install cheatsheet
+fi
 
 # for hobby
 if [ $OSX_PRIVATE ]; then
     brew cask install skype
-    brew cask install xquartz
-    brew cask install xbox360-controller-driver
     brew cask install blender
     brew cask install gimp
     brew cask install inkscape
     brew cask install minecraft
     brew cask install steam
     brew cask install dropbox
-    brew cask install cooviewer
-#    mediahuman
-#    mikutter
-#    snes
-#    grafittipod
+
+    if [[ $OSTYPE == darwin* ]]; then
+        brew cask install xquartz
+        brew cask install cooviewer
+        brew cask install xbox360-controller-driver
+#        mediahuman
+#        mikutter
+#        snes
+#        grafittipod
+    fi
 fi
 
 brew cask cleanup
